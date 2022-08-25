31681789d0fa7ca997a102d30bc393dd328090d4
stanleyamate <amatestanley08@gmail.com>
2022-08-25 19:15:27 +0100

    updated v2

--- landing-page-ui/assets/js/app.js
+++ landing-page-ui/assets/js/app.js
@@ -1,4 +1,5 @@
 
+
 const body = document.querySelector('body');
 const hamburger = document.querySelector('.hamburger')
   hamburger.onclick = function () {
@@ -9,4 +10,32 @@ const hamburger = document.querySelector('.hamburger')
     else{
       body.classList.remove('active')
     }
+    
 }
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
